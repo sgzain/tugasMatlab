e2a5a6b5562c337853af6a2d75adb305186f845f
sgzain <45408888+sgzain@users.noreply.github.com>
2021-08-20 17:11:08 +0800

    program gabung nama
    Menggabungkan nama depan belakang dengan kode password

---
+++ nama_depan_belakang.m
@@ -0,0 +1,20 @@
+[num, txt] = xlsread('KECERDASAN BUATAN.xlsx','C9:C40');
+[num, txt2] = xlsread('KECERDASAN BUATAN.xlsx','D9:D40');
+txt3 = xlsread('KECERDASAN BUATAN.xlsx','B9:B40');
+nn = length(txt);
+ffd = [];
+for k = 1 : nn;
+data=txt(k);
+data1 = cell2mat(data);
+str = sscanf(data1, '%s',1 );
+n = length(str);
+str2 = sscanf(data1(n+2:end), '%s',1 );
+s = randi([35 38]);s =char(s);
+s1 = randi([48 57]);s1=char(s1);
+s3 = randi([65 90]);s3=char(s3);
+s4 = randi([97 122],1,5);s4=char(s4);
+d = strcat(s,s1,s3,s4);
+ffd1 = [txt3(k) {d} {str} {str2} txt2(k)];
+sd = strcat('A',num2str(k+1));
+xlswrite('KECERDASAN BUATAN.csv', ffd1,'Sheet1',sd);
+end
